1c1bb98e50e9bd7cce343082e622989d7a2981ed
root <yahya.left@gmail.com>
2018-05-20 11:21:31 +0700

    Create dummy users and posts table

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // reset the users table
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('users')->truncate();
+
+        //generate 3 users/author
+        DB::table('users')->insert([
+        	[
+        		'name' => "John Doe",
+        		'email' => "johndoe@test.com",
+        		'password' => bcrypt('secret')
+        	],
+        	[
+        		'name' => "Jane Doe",
+        		'email' => "janedoe@test.com",
+        		'password' => bcrypt('secret')
+        	],
+        	[
+        		'name' => "Johny Doe",
+        		'email' => "johnydoe@test.com",
+        		'password' => bcrypt('secret')
+        	],
+        ]);
+    }
+}
